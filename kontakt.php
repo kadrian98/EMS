df1bc99d3d2eff3841446715318c31716bd31187
Adrian Kaczmarek <adrian.kaczmarek@web-solutions.eu>
2023-10-30 11:26:16 +0100

    change contact info

--- kontakt.php
+++ kontakt.php
@@ -12,7 +12,7 @@ get_header();
         ></span>
         <h1 class="socials__social_header">Email</h1>
         <p class="socials__social_para">
-          <a href="mailto:ftp@strefaems.pl">ftp@strefaems.pl</a>
+          <a href="mailto:kontakt@strefaems.pl">kontakt@strefaems.pl</a>
         </p>
       </div>
       <div class="socials__social">
@@ -21,7 +21,7 @@ get_header();
         ></span>
         <h1 class="socials__social_header">Telefon</h1>
         <p class="socials__social_para">
-          <a href="tel:+4733378901">+47 333 78 901</a>
+          <a href="tel:+48690342535">+48 690 342 535 </a>
         </p>
       </div>
       <div class="socials__social">
@@ -29,23 +29,11 @@ get_header();
           ><i class="fa-solid fa-location-dot"></i
         ></span>
         <h1 class="socials__social_header">Adres</h1>
-        <p class="socials__social_para">EMS Sport</p>
-        <p class="socials__social_para">Adres 71/2</p>
-        <p class="socials__social_para">11-222 Szczecin</p>
+        <p class="socials__social_para">Strefa EMS</p>
+        <p class="socials__social_para">Nowy Turzyn</p>
+        <p class="socials__social_para">Aleja Bohaterów Warszawy 40</p>
       </div>
-      <div style="width: 100%; margin-bottom: 3rem">
-        <iframe
-          width="100%"
-          height="400"
-          frameborder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-          src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Ems%20sport%20szczecin+(EMS%20Sport%20Szczecin)&amp;t=k&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-          ><a href="https://www.maps.ie/population/"
-            >Population Estimator map</a
-          ></iframe
-        >
+      <div style="width: 100%"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Strefa%20EMS%20Aleja%20Bohater%C3%B3w%20Warszawy%2040+(Strefa%20EMS)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Find Population on Map</a></iframe></div>
       </div>
     </section>
     <?php get_footer(); ?>
